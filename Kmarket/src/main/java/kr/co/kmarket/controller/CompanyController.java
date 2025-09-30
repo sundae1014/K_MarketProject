e16e985426df42750310dfd9285b1f25c1640146
sundae1014 <link8430@gmail.com>
2025-09-30 14:08:48 +0900

    Kmarket 통합

---
+++ Kmarket/src/main/java/kr/co/kmarket/controller/CompanyController.java
@@ -0,0 +1,35 @@
+package kr.co.kmarket.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/company")
+public class CompanyController {
+
+    @GetMapping("/culture")
+    public String culture(){
+        return "company/culture";
+    }
+
+    @GetMapping("/index")
+    public String index(){
+        return "company/index";
+    }
+
+    @GetMapping("/media")
+    public String media(){
+        return "company/media";
+    }
+
+    @GetMapping("/recruit")
+    public String recruit(){
+        return "company/recruit";
+    }
+
+    @GetMapping("/story")
+    public String story(){
+        return "company/story";
+    }
+}
---
+++ Kmarket/src/main/java/kr/co/kmarket/controller/MemberController.java
@@ -0,0 +1,55 @@
+package kr.co.kmarket.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+
+    @GetMapping("/join")
+    public String join(){
+        return "member/join";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "member/login";
+    }
+
+    @GetMapping("/register")
+    public String register(){
+        return "member/register";
+    }
+
+    @GetMapping("/registerSeller")
+    public String registerSeller(){
+        return "member/registerSeller";
+    }
+
+    @GetMapping("/signup")
+    public String signup(){
+        return "member/signup";
+    }
+
+    @GetMapping("/find/changePassword")
+    public String changePassword(){
+        return "member/find/changePassword";
+    }
+
+    @GetMapping("/find/password")
+    public String password(){
+        return "member/find/password";
+    }
+
+    @GetMapping("/find/resultId")
+    public String resultId(){
+        return "member/find/resultId";
+    }
+
+    @GetMapping("/find/userId")
+    public String userId(){
+        return "member/find/userId";
+    }
+}
