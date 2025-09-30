a39feaebf0b75864200d1ed9ee8f50d29c8566c4
o-seoj <tjwjd010@naver.com>
2025-09-30 20:58:00 +0900

    add 통합본 회사소개, 회원, 관리자 헤더푸터 추가

---
+++ Kmarket/src/main/java/kr/co/kmarket/controller/admin/AdminMainController.java
@@ -0,0 +1,14 @@
+package kr.co.kmarket.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminMainController {
+    @GetMapping("/admin")
+    public String culture(){
+        return "admin/admin";
+    }
+
+}
