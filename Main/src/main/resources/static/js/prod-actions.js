d8a500bbe6ee1379ec4df568ae0c2dcffd757945
sub6550 <sub6550@gmail.com>
2025-10-02 14:07:38 +0900

    상품 주문완료 프론트 구현 중

--- Main/src/main/resources/static/js/prod-actions.js
+++ Main/src/main/resources/static/js/prod-actions.js
@@ -3,6 +3,7 @@ const btnWish = document.querySelector(".btn.wish");
 const btnCart = document.querySelector(".btn.cart");
 const btnBuy  = document.querySelector(".btn.buy");
 
+/* 찜 기능 */
 btnWish.addEventListener("click", () => {
     if (userType === "guest") {
         // 비로그인 → 로그인 페이지
@@ -13,14 +14,54 @@ btnWish.addEventListener("click", () => {
     }
 });
 
+/* 장바구니 기능 */
 btnCart.addEventListener("click", () => {
     if (userType === "guest") {
-        location.href = "/login";
+        if (confirm("로그인이 필요한 서비스입니다.\n로그인 페이지로 이동하시겠습니까?")) {
+            location.href = "/cupang/login"; // context-path 포함
+        }
     } else {
-        location.href = "/cart";
+        // 옵션 데이터 가져오기
+        const optionEl = document.querySelector("#opt");
+        const selectedOpt = optionEl.options[optionEl.selectedIndex];
+        if (!selectedOpt.value) {
+            alert("옵션을 선택해주세요!");
+            return;
+        }
+
+        const name = document.querySelector(".prod-title").textContent;
+        const qty = parseInt(document.querySelector(".prod-selected input").value) || 1;
+        const price = parseInt(selectedOpt.dataset.price);
+        const discount = 0;
+
+        // 기존 장바구니 불러오기
+        let cartData = JSON.parse(localStorage.getItem("cartData")) || [];
+
+        // 중복 여부 확인
+        const exist = cartData.find(i => i.id === selectedOpt.value);
+        if (exist) {
+            exist.qty += qty;
+        } else {
+            cartData.push({
+                id: selectedOpt.value,
+                name: name + " - " + selectedOpt.text,
+                price: price,
+                discount: discount,
+                qty: qty
+            });
+        }
+
+        // localStorage 갱신
+        localStorage.setItem("cartData", JSON.stringify(cartData));
+
+        // 알림창
+        if (confirm("상품이 장바구니에 담겼습니다.\n장바구니로 이동하시겠습니까?")) {
+            location.href = "/cupang/prodCart";  // ✅ context-path 포함
+        }
     }
 });
 
+/* 바로구매 기능 */
 btnBuy.addEventListener("click", () => {
     if (userType === "guest") {
         location.href = "/login";
---
+++ Product/src/main/java/kr/co/cupang/controller/ProdOrderController.java
@@ -0,0 +1,13 @@
+package kr.co.cupang.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProdOrderController {
+
+    @GetMapping(("/prodOrder"))
+    public String prodOrder() {
+        return "prodOrder";
+    }
+}
