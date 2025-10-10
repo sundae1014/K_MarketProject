4f89c5a6f6254b630feccfa380f79e4173d5cb0f
o-seoj <tjwjd010@naver.com>
2025-10-10 18:25:27 +0900

    관리자&회사소개 채용관리(페이징 제외 완료), 관리자 메인 회원가입, 접속자수 추가

--- Kmarket/src/main/java/kr/co/kmarket/controller/GlobalController.java
+++ Kmarket/src/main/java/kr/co/kmarket/controller/GlobalController.java
@@ -1,8 +1,11 @@
 package kr.co.kmarket.controller;
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import kr.co.kmarket.dto.MemberDTO;
 import kr.co.kmarket.security.MyUserDetails;
+import kr.co.kmarket.service.PageCounterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -17,6 +20,24 @@ public class GlobalController {
             return myMember.getMemberDTO();
         }
         return new MemberDTO();
+    }
+
+    private final PageCounterService counterService;
+
+    @ModelAttribute("visitorTotal")
+    public int visitorTotal(HttpServletRequest request, HttpServletResponse response) {
+        counterService.countVisit(request, response);
+        return counterService.getTotalVisitsLastTwoDays();
+    }
+
+    @ModelAttribute("visitorToday")
+    public int visitorToday() {
+        return counterService.getTodayVisits();
+    }
+
+    @ModelAttribute("visitorYesterday")
+    public int visitorYesterday() {
+        return counterService.getYesterdayVisits();
     }
 
 }
--- Kmarket/src/main/java/kr/co/kmarket/mapper/admin/RecruitMapper.java
+++ Kmarket/src/main/java/kr/co/kmarket/mapper/admin/RecruitMapper.java
@@ -9,4 +9,14 @@ import java.util.List;
 @Mapper
 public interface RecruitMapper {
     public List<HireDTO> selectAllHireList();
+
+    public void deleteAllByHireNo(List<Integer> hire_no);
+
+    public void insertHire(HireDTO hireDTO);
+
+    public void updateExpiredHires();
+
+    public List<HireDTO> selectSearch(String searchType, String keyword);
+
+
 }
