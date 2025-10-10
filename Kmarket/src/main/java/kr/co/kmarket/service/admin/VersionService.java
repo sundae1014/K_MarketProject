1cf22ceabed13d0a381e8bb2de64dc80f8c10341
whddls0323 <whddls0323@naver.com>
2025-10-10 11:17:05 +0900

    관리자 버전관리 제작완료

--- Kmarket/src/main/java/kr/co/kmarket/service/admin/VersionService.java
+++ Kmarket/src/main/java/kr/co/kmarket/service/admin/VersionService.java
@@ -1,5 +1,7 @@
 package kr.co.kmarket.service.admin;
 
+import kr.co.kmarket.dto.PageRequestDTO;
+import kr.co.kmarket.dto.PageResponseDTO;
 import kr.co.kmarket.dto.VersionDTO;
 import kr.co.kmarket.mapper.admin.VersionMapper;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +17,22 @@ public class VersionService {
     private final VersionMapper versionMapper;
 
     public void save(VersionDTO versionDTO) {versionMapper.insert(versionDTO);}
-    public List<VersionDTO> selectAll() {return versionMapper.findAll();}
+    public VersionDTO getVersion(String versionId) {return versionMapper.findById(versionId);}
+
+    public PageResponseDTO selectAll(PageRequestDTO pageRequestDTO) {
+        List<VersionDTO> dtoList = versionMapper.findAll(pageRequestDTO);
+
+        int total =versionMapper.selectCountTotal(pageRequestDTO);
+
+        return PageResponseDTO.<VersionDTO>builder()
+                .pageRequestDTO(pageRequestDTO)
+                .dtoList(dtoList)
+                .total(total)
+                .build();
+    }
+    public int selectCountTotal(PageRequestDTO pageRequestDTO) {
+        return versionMapper.selectCountTotal(pageRequestDTO);
+    }
+
     public void remove (List<Long> idList) {versionMapper.delete(idList);}
 }
