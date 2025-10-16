5e781d2f8072b68230d808f63cdb3ec077d57e80
wjsdydwns01 <jjyyjj118@naver.com>
2025-10-16 16:50:27 +0900

    쿠폰 list완

--- Kmarket/src/main/java/kr/co/kmarket/mapper/admin/CouponMapper.java
+++ Kmarket/src/main/java/kr/co/kmarket/mapper/admin/CouponMapper.java
@@ -11,8 +11,20 @@ public interface CouponMapper {
     int insertCoupon(CouponDTO couponDTO);
     List<CouponDTO> selectCoupons();
 
+    //검색 기능
     List<CouponDTO> selectSearchCoupons(@Param("type") String type,
                                         @Param("keyword") String keyword);
+
+    //페이지네이션
+    List<CouponDTO> selectCouponsPage(@Param("type") String type,
+                                      @Param("keyword") String keyword,
+                                      @Param("offset") int offset,
+                                      @Param("size") int size);
+
+    int countCoupons(@Param("type") String type,
+                     @Param("keyword") String keyword);
+
+
 }
 
 
